7edd712431368cdbf3fb61da42bd7aecbce8c447
yezustheoneandonly <wmountain.entertainment@googlemail.com>
2023-09-19 10:55:08 +0200

    finished part4 and add part4 to index.js

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
-// IMPORTE EURER FUNCTIONS
-
-
-
-
-
-// START INPUT
-// const input = "s?§$f5wa&$=6eb8Ad1hs12z1,89i4w&/ajkhuyPs5§irf95kfoguh6(&opg%fh";
-
-const codeAndSecret = (input) => {
-  // FUNCTION AUS TEIL 1
-
-  // FUNCTION AUS TEIL 2
-
-  // FUNCTION AUS TEIL 3
-
-  // FUNCTION AUS TEIL 4
-
-
+// Teil 1: PROGRAMM-PART A: Bereinigung codierter Nachrichten
+function cleanInput(input) {
+  // Code 
 }
-/* console.log(codeAndSecret(input)); */
+
+// Teil 2: PROGRAMM-PART B: Buchstaben verschieben
+function shiftLetters(input) {
+  // Code 
+}
+
+// Teil 3: PROGRAMM-PART C: Ziffern-Codierung
+function encodeDigits(input) {
+  // Code 
+}
+
+// Teil 4: PROGRAMM-PART D: Schlüssel-Codierung
+function encodeKey(inputArray) {
+  
+  for (let i = 0; i < inputArray.length; i++) {
+    // links von "A"
+    let leftElement = inputArray[i - 2] ?? -Infinity;
+    // rechts von "A"
+    let rightElement = inputArray[i + 2] ?? -Infinity;
+    
+    // Wenn "A" <= "V" ist, => "A" + 10
+    if (inputArray[i] <= leftElement || inputArray[i] <= rightElement) {
+        inputArray[i] += 10;
+    }
+}
+
+return inputArray;
+}
+
+
+function codeAndSecret(input) {
+  let partAOutput = cleanInput(input);
+  let partBOutput = shiftLetters(partAOutput);
+  let partCOutput = encodeDigits(partBOutput);
+  let finalOutput = encodeKey(partCOutput);
+  
+  return finalOutput;
+}
+
+const input = "s?§$f5wa&$=6eb8Ad1hs12z1,89i4w&/ajkhuyPs5§irf95kfoguh6(&opg%fh";
+console.log(codeAndSecret(input)); // Sollte [8, 18, 17, 10,  8, 14, 13, 12, 11, 9,  4, 12, 12, 15, 12, 15,  7, 12] ausgeben
--- part4.js
+++ part4.js
@@ -1,5 +1,25 @@
 // BEISPIEL INPUT
 // const inputAusTeil3 = [8, 8, 7, 10, 8, 4, 3, 2, 1, 9, 4, 2, 2, 5, 2, 5, 7, 2];
 
+function encodeKey(inputArray) {
+    for (let i = 0; i < inputArray.length; i++) {
+        // links von "A"
+        let leftElement = inputArray[i - 2] ?? -Infinity;
+        // rechts von "A"
+        let rightElement = inputArray[i + 2] ?? -Infinity;
+        
+        // Wenn "A" <= "V" ist, => "A" + 10
+        if (inputArray[i] <= leftElement || inputArray[i] <= rightElement) {
+            inputArray[i] += 10;
+        }
+    }
+    
+    return inputArray;
+}
+
+const inputAusTeil3 = [8, 8, 7, 10, 8, 4, 3, 2, 1, 9, 4, 2, 2, 5, 2, 5, 7, 2];
+console.log(encodeKey(inputAusTeil3)); // Sollte [8, 18, 17, 10,  8, 14, 13, 12, 11, 9,  4, 12, 12, 15, 12, 15,  7, 12] ausgeben
+
+
 
 
