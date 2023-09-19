e89e8196799d37f431a9801f1ce4dd13521a4399
TwinMine <michaelrolo11@gmail.com>
2023-09-19 11:49:40 +0200

    .

--- part2.js
+++ part2.js
@@ -1,3 +1,29 @@
 // BEISPIEL INPUT
-// const inputAusTeil1 = "pgorPyukjizhdAbeawfs"
 
+
+
+
+//  "b", "d", "e", "v", "w" 
+
+
+function shiftLetters(input) {
+    
+    let shiftedInput = input.replace(/[a-z]/gi, function(c) {
+        switch (c) {
+            case 'b': case 'B': case 'd': case 'D': case 'e': case 'E': case 'v': case 'V': case 'w': case 'W':
+                return String.fromCharCode(c.charCodeAt(0) + 1);
+            default:
+                return String.fromCharCode(c.charCodeAt(0) + 2);
+        }
+    });
+    
+    
+    shiftedInput = shiftedInput.replace(/[^a-zA-Z]/g, '');
+    
+    
+    let reversedInput = shiftedInput.split('').reverse().join('');
+
+    return reversedInput;
+}
+
+console.log(shiftLetters("pgorPyukjizhdAbeawfs"));
